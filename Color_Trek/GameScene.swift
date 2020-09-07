1a37222f1e90542061482b0d5481e7f6c7fa4da7
ProdigyX6217 <dominic.holder.15@cityas.org>
2020-09-07 17:43:52 -0600

    Added Pause Feature

--- Color_Trek/GameScene.swift
+++ Color_Trek/GameScene.swift
@@ -25,6 +25,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
 //      HUD
+    var pause:SKSpriteNode?
     var timeLabel:SKLabelNode?
     var scoreLabel:SKLabelNode?
     
@@ -67,6 +68,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     func createHUD () {
+        pause = self.childNode(withName: "pause") as? SKSpriteNode
         timeLabel = self.childNode(withName: "time") as? SKLabelNode
         scoreLabel = self.childNode(withName: "score") as? SKLabelNode
         
@@ -342,11 +344,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let node = self.nodes(at: location).first
             
             if node?.name == "right" {
-                moveToNextTrack()
+                if currentTrack < 8 {
+                    moveToNextTrack()
+                }
             } else if node?.name == "up"{
                 moveVertically(up: true)
             } else if node?.name == "down" {
                 moveVertically(up: false)
+            } else if node?.name == "pause", let scene = self.scene {
+                if scene.isPaused {
+                    scene.isPaused = false
+                }else {
+                    scene.isPaused = true
+                }
             }
         }
     }
